1348b5792c3411d6802afdb4a09e496eb2fd6d58
Matthieu Lepinay <lepinay.matthieu@gmail.com>
2012-11-13 21:39:51 +0100

    First Commit

---
+++ MapReader/MapReader/Game1.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using FarseerPhysics.Collision;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Common;
+using FarseerPhysics.Common.ConvexHull;
+using FarseerPhysics.Common.Decomposition;
+using FarseerPhysics.Common.PhysicsLogic;
+using FarseerPhysics.Common.PolygonManipulation;
+using FarseerPhysics.Controllers;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Dynamics.Contacts;
+using FarseerPhysics.Dynamics.Joints;
+using FarseerPhysics.Factories;
+
+namespace MapReader
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            readFromFile("level1.txt");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+
+        public void readFromFile(String path)
+        {
+            int x = 0;
+            int y = 0;
+            List<Body> bodies = new List<Body>();
+            World world = new World(new Vector2(0, 300));
+
+            string[] readText = System.IO.File.ReadAllLines(path);
+            foreach (string s in readText)
+            {
+                x = 0;
+                while (x < s.Length)
+                {
+                    if (s[x] != '0')
+                    {
+                        Body bodyTemp = BodyFactory.CreateRectangle(world, 32, 32, 1f);
+                        bodyTemp.BodyType = BodyType.Static;
+                        bodyTemp.Position = new Vector2(x * 32, y * 32);
+
+                        bodies.Add(bodyTemp);
+                    }
+                    ++x;
+                }
+                ++y;
+            }
+        }
+    }
+}
---
+++ MapReader/MapReader/Platform.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapReader
+{
+    class Platform
+    {
+
+        public int m_pos_x = 0;
+        public int m_pos_y = 0;
+
+        public Platform(int pos_x, int pos_y)
+        {
+            m_pos_x = pos_x;
+            m_pos_y = pos_y;
+        }
+
+    }
+}
